da0b7ee6854dfebf62298f993dec62a236d977db
Alexander Kharkevich <alexander_kharkevich@epam.com>
2022-11-12 19:22:37 -0500

    debug

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 
-
 // function to get issue comments
 async function getIssueComments(octokit, owner, repo, issue_number) {
     response = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments{?since,per_page,page}', {
@@ -10,30 +9,43 @@ async function getIssueComments(octokit, owner, repo, issue_number) {
         repo: repo,
         issue_number: issue_number
     });
-    // const response = await octokit.issues.listComments({
-    //     owner,
-    //     repo,
-    //     issue_number
-    // });
     return response.data;
 }
 
+// function to get team members
+async function getTeamMembers(octokit, org, team_slug) {
+    response = await octokit.request('GET /orgs/{org}/teams/{team_slug}/members', {
+        org: org,
+        team_slug: team_slug
+    });
+    return response.data;
+}
+
+
 async function main() {
     try {
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
+        // const time = (new Date()).toTimeString();
+        // core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
+        const octokit = new github.getOctokit(core.getInput('repo-token'));
+        const mode = core.getInput('mode');
+        let team_members;
+        if (mode === 'team') {
+            team_members = await getTeamMembers(octokit, github.context.repo.owner, core.getInput('approvers'));
+            console.log(JSON.stringify(team_members, undefined, 2));
+            // core.setOutput("team_members", team_members);
+        } else {
+            team_members = core.getInput('approvers').split(',');
+        }
         const payload = JSON.stringify(github.context.payload, undefined, 2)
         console.log(`The event payload: ${payload}`);
         const issue_number = github.context.payload.issue.number;
         console.log(`The issue number is: ${issue_number}`);
         console.log(`The repo is: ${github.context.repo.repo}`);
         console.log(`The owner is: ${github.context.repo.owner}`);
-        const octokit = new github.getOctokit(core.getInput('repo-token'));
+
         const comments = await getIssueComments(octokit, github.context.repo.owner, github.context.repo.repo, issue_number);
+        console.log(Array.isArray(comments));
         console.log(`The comments are: ${JSON.stringify(comments, undefined, 2)}`);
     } catch (error) {
         core.setFailed(error.message);
