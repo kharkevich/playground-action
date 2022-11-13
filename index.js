2b9e4cc90f7dbfc57fc09bdf239634fe396b4a0c
Alexander Kharkevich <alexander_kharkevich@epam.com>
2022-11-12 22:29:03 -0500

    debug

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ async function main() {
         const decline_words = core.getInput('decline-words').split(',');
         const approve_words = core.getInput('approve-words').split(',');
         core.info(`Decline words: ${decline_words}`);
+        console.log(decline_words);
         console.log(Array.isArray(decline_words));
         core.info(`Approve words: ${approve_words}`);
         console.log(Array.isArray(approve_words));
